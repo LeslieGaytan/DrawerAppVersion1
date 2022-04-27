3883817ad0bb8a2c98dd1bdf2a494d148ce8930c
FlutLab.io <>
2022-04-27 14:25:01 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practica Drawer"),
+        actions: [
+          Icon(Icons.search),
+          Icon(Icons.more_vert)
+        ],
+        backgroundColor: Colors.red[900],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/alumno4.png'),
+          Image.asset('assets/alumno5.png'),
+          Image.asset('assets/alumno6.png'),
+          // Image.asset('images/pic2.jpg'),
+          // Image.asset('images/pic3.jpg'),
+        ],
+      ),
+      drawer: Drawer(
+        // Agrega un ListView al drawer. Esto asegura que el usuario pueda desplazarse
+        // a través de las opciones en el Drawer si no hay suficiente espacio vertical
+        // para adaptarse a todo.
+        child: ListView(
+          // Importante: elimina cualquier padding del ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Encabezado del Drawer'),
+              decoration: BoxDecoration(
+                color: Colors.red[300],
+              ),
+            ),
+            ListTile(
+              title: Text('Alumno 1'),
+              onTap: () {
+                // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Alumno 2'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Alumno 3'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
